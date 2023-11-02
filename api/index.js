517b3c06b8e9f66309d70a7ff48d957579a4fdf6
Goldy <sunnychaudhary1811@gmail.com>
2023-11-02 11:41:23 +0530

    connected to Mongo DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express'
+import { MongoServerClosedError } from 'mongodb';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connected to mongo")
+}).catch((err) => {
+    console.log("mongo err", err)
+})
+
 
 const app = express();
 
